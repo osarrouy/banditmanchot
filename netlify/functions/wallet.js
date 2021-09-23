39d00947b000662643fd8bc4a25502e6c5131130
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2021-09-23 12:18:41 +0200

    Fix bug

--- netlify/functions/wallet.js
+++ netlify/functions/wallet.js
@@ -1,10 +1,8 @@
 const { ethers } = require('ethers');
 
-
 exports.handler = async function (event, context) {
   const provider = ethers.getDefaultProvider('mainnet', { infura: { projectId: 'ac60a83411294c00b1f6a91aa340b65d' } });
   const wallet = ethers.Wallet.createRandom().connect(provider);
-
 
   return {
     statusCode: 200,
@@ -13,5 +11,6 @@ exports.handler = async function (event, context) {
       publicKey: wallet.publicKey,
       privateKey: wallet.privateKey,
       balalnce: (await wallet.getBalance()).toString(),
-    })
+    }),
+  };
 };
