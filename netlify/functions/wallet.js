c75e1d356065a5b4de0ade6e7ad4d27e2f4db53f
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2021-09-23 15:28:14 +0200

    Add a bingo field

--- netlify/functions/wallet.js
+++ netlify/functions/wallet.js
@@ -1,8 +1,10 @@
 const { ethers } = require('ethers');
+const ZERO = ethers.BigNumber.from(0);
 
 exports.handler = async function (event, context) {
   const provider = ethers.getDefaultProvider('mainnet', { infura: { projectId: 'ac60a83411294c00b1f6a91aa340b65d' } });
   const wallet = ethers.Wallet.createRandom().connect(provider);
+  const balance = await wallet.getBalance();
 
   return {
     statusCode: 200,
@@ -10,7 +12,8 @@ exports.handler = async function (event, context) {
       address: wallet.address,
       publicKey: wallet.publicKey,
       privateKey: wallet.privateKey,
-      balance: ethers.utils.formatEther(await wallet.getBalance()),
+      balance: ethers.utils.formatEther(balance),
+      bingo: balance.gt(ZERO),
     }),
   };
 };
